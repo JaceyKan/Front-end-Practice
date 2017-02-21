160610fd29b9c31b8bdd88195aab2f1342bf41be
JaceyKan <jacey_kan@sina.com>
2017-02-21 23:02:22 +0800

    add JaceyKanResum.html

---
+++ js/JaceyKanResume.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#portfolio .work a").mouseenter(function(){
+		$(this).find(".showImg").css("opacity","0.4");
+		$(this).find(".coverImg").css("display","inline");
+	});
+
+	$("#portfolio .work a").mouseleave(function(){
+		$(this).find(".showImg").css("opacity","1");
+		$(this).find(".coverImg").css("display","none");
+	});
+});
