d34968a5a258916d0f192160c35fec6d8871b1aa
JaceyKan <jacey_kan@sina.com>
2017-02-22 11:01:04 +0800

    updat JaceyKanResume

--- js/JaceyKanResume.js
+++ js/JaceyKanResume.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 	$("#portfolio .work a").mouseenter(function(){
-		$(this).find(".showImg").css("opacity","0.4");
-		$(this).find(".coverImg").css("display","inline");
+		//$(this).find(".showImg").css("opacity","0.4");
+		//$(this).find(".coverImg").css("display","inline");
+		//$(this).find(".showImg").css("opacity","0.4");
+		$(this).find(".coverImg").css({"display":"inline","opacity":"0.7"});
 	});
 
 	$("#portfolio .work a").mouseleave(function(){
-		$(this).find(".showImg").css("opacity","1");
+		//$(this).find(".showImg").css("opacity","1");
 		$(this).find(".coverImg").css("display","none");
 	});
 });
